bc1c95378eb0f26243067c357b8a18fd42b54b34
Zach Alaniz <zachalaniz@Zachs-MacBook-Pro.local>
2017-08-14 19:26:44 -0600

    Added possible logo

--- public/js/mobile-nav.js
+++ public/js/mobile-nav.js
@@ -1,8 +1,14 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("brand").style.opacity = ".5";
+
 }
+
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("brand").style.opacity = "1";
 }
