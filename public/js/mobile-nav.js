287a800d9b56b9e2deedf0d6b22dd3a513eb479f
Zach Alaniz <zachalaniz@Zachs-MacBook-Pro.local>
2017-08-16 19:20:39 -0600

    Added top of homepage and introduction section of homepage

--- public/js/mobile-nav.js
+++ public/js/mobile-nav.js
@@ -1,14 +1,13 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  document.getElementById("brand").style.opacity = ".5";
+  //document.body.style.backgroundColor = "rgba(0,0,0,.4)";
+  //document.getElementById("brand").style.opacity = ".5";
 
 }
-
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.body.style.backgroundColor = "white";
-  document.getElementById("brand").style.opacity = "1";
+//  document.body.style.backgroundColor = "white";
+  //document.getElementById("brand").style.opacity = "1";
 }
